3d0b74c2f7ff27d3792961d204b8919bec0e42b4
purohr <diego683@gmail.com>
2022-08-03 22:43:08 -0500

    views list and create

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views import generic
+from .models import Post
+
+
+class PostList(generic.ListView):
+    """
+    Return all posts that are with status 1 (published) and order from the latest one.
+    """
+
+    queryset = Post.objects.filter(status=1).order_by("-created_at")
+    template_name = "index.html"
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = "post_detail.html"
