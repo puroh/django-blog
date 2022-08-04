900e02b9e4eefd04615de7013be998150a8c03d9
purohr <diego683@gmail.com>
2022-08-03 23:32:00 -0500

    rename blog folder

--- blog/urls.py
+++
@@ -1,5 +0,0 @@
-from . import views
-from django.urls import pathurlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(),  name='post_detail'),
-]
