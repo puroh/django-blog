eff771368100374e49a9e13857219fc103748507
purohr <diego683@gmail.com>
2022-08-04 01:21:42 -0500

    add model category

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from djangoblog.mixins import MixinComunFields
+
+
+class Category(MixinComunFields):
+    title = models.CharField(max_length=20)
+    subtitle = models.CharField(max_length=20)
+    slug = models.SlugField()
+    thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.title
